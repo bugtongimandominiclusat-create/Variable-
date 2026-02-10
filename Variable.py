757487625b15d8ab324ea4eded6a6667e9bd43bd
bugtongimandominiclusat-create <bugtong.imandominiclusat@gmail.com>
2026-02-10 19:19:50 +0800

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,16 @@
+fullname = "Andrew Josie A. Suan"        
+age = 22                 
+height = 1.75                
+working = False           
+   
+print("Fullname:", fullname) 
+print(type(fullname)) 
+
+print("\nAge: ",age)
+print(type(age))
+
+print("\nHeight in meters: ",height)
+print(type(height))
+
+print("\nCurrently Working?",working)
+print(type(working))
